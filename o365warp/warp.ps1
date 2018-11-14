91087c6a78bee44f3a2942b87b4f5f474a452bc1
AkiHelkio <aki.helkio@gmail.com>
2018-11-14 15:19:13 +0200

    Implemented better checking of selected filter

--- o365warp/warp.ps1
+++ o365warp/warp.ps1
@@ -20,21 +20,28 @@ Connect-MsolService -Credential $creds
 # List your partner contracts
 $data = Get-MsolPartnerContract
 # Show them
+write-host "All available tenants:" -foreground cyan
 $data | select Name, DefaultDomainName, @{n="TenantId";e={$_.TenantId.Guid}} | ft -a
 
 # Select one by ps1 param:
-$selected = $data | where { $_.Name -like "*$TenantName*" } | select -first 1
-if ($selected -ne $null){
-  # Open new connection window
-  write-host "Filtered result" -foreground cyan
-  $selected |ft -a
-  write-host "Continue (y) ?" -foreground yellow
-  $c = $host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
-  if ($c.Character -eq "y") {
-    Start-Process powershell -ArgumentList "-noexit",".\new-tenantsession.ps1 -TenantDomainName $($selected.DefaultDomainName)"
+$selected = @($data | where { $_.Name -like "*$TenantName*" })
+if ($selected.count -gt 0){
+  # Provide debug:
+  write-host "Filtered results:" -foreground cyan
+  $selected | select Name, DefaultDomainName |ft -a
+  if ($selected.count -eq 1) {
+    write-host "Continue with connection to tenant (y) ?" -foreground yellow
+    $c = $host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
+    if ($c.Character -eq "y") {
+      # Open new connection window
+      Start-Process powershell -ArgumentList "-noexit",".\new-tenantsession.ps1 -TenantDomainName $($selected.DefaultDomainName)"
+    }
+  }
+  else {
+    write-host "Multiple search results. Please specify which name to connect to" -foreground yellow
   }
 }
 else {
-  write-host "No tenant found with filter" -foreground red -background black
+  write-host "No tenant found with filter" -foreground red
 }
 
