dd7869ef422026c7c9f27897ac495c18e8fc94fb
AkiHelkio <aki.helkio@gmail.com>
2018-11-15 23:25:44 +0200

    Added support for testing

--- scripts/o365warp/new-tenantsession.ps1
+++ scripts/o365warp/new-tenantsession.ps1
@@ -4,11 +4,15 @@ Connects to o365 tenant domain with own credentials
 #>
 [CmdletBinding()]
 param(
-[String]$TenantDomainName=""
+[String]$TenantDomainName="",
+[String]$Mode="Production"
 )
 $Host.UI.RawUI.WindowTitle = "o365 Connection to '$TenantDomainName'"
 write-host "Connecting to: '$TenantDomainName'"
 
+if ( $Mode -eq "Test" ) {
+  return
+}
 $credentials = get-credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell-liveid?DelegatedOrg=$TenantDomainName -Credential $credentials -Authentication Basic -AllowRedirection
 Import-PSSession $Session
